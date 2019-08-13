273a1ca13c1aa0e3b37829fa948d7edf39c683ef
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-08-13 15:18:50 +0700

    change data migration, allow data field nullable

--- database/migrations/2019_08_13_081518_alter_documents_allow_data_nullable.php
+++ database/migrations/2019_08_13_081518_alter_documents_allow_data_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterDocumentsAddDataDefault extends Migration
+class AlterDocumentsAllowDataNullable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class AlterDocumentsAddDataDefault extends Migration
     {
         Schema::table('documents', function (Blueprint $table) {
             //
-            $table->json('data')->default('{}')->change();
+            $table->json('data')->nullable()->change();
         });
     }
 
