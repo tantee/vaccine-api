148152d2cb2e87f98cdd2c1a4e17011dfe96c228
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-08-14 08:24:35 +0700

    debug toStores variable

--- app/Models/Traits/StoreToAsset.php
+++ app/Models/Traits/StoreToAsset.php
@@ -6,8 +6,6 @@ use Log;
 use App\Http\Controllers\Asset\AssetController;
 
 trait StoreToAsset {
-  protected $toStores = array();
-  
   public static function bootStoreToAsset() {
     static::creating(function($model) {
       $model->storeWhenSave();
@@ -18,14 +16,14 @@ trait StoreToAsset {
   }
 
   public function storeWhenSave() {
-    $toStores = $this->$toStores;
+    $toStores = (isset($this->$toStores)) ? $this->$toStores : [];
     foreach($toStores as $toStore) {
       if (isset($model->$toStore) && \is_array($model->$toStore)) {
         if (isset($model->hn)) \array_walk($model->$toStore,[$this,'storeToAsset'],$model->hn);
       }
     }
   }
-  
+
   protected function storeToAsset(&$modelValue,$modelKey,$hn) {
     if (\is_array($modelValue) && isset($modelValue['base64string']) && !isset($modelValue['id'])) {
       if (isset($modelValue['assetType'])) $assetType = $modelValue['assetType'];
