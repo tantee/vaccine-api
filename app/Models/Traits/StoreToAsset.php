629b573611daad993039cce53149fdb177a1dd19
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-08-17 10:44:18 +0700

    Unarray storeToAsset result

--- app/Models/Traits/StoreToAsset.php
+++ app/Models/Traits/StoreToAsset.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\Traits;
 
-use Log;
 use App\Http\Controllers\Asset\AssetController;
 
 trait StoreToAsset {
@@ -19,7 +18,6 @@ trait StoreToAsset {
     $toStores = (isset($this->toStores)) ? $this->toStores : [];
     foreach($toStores as $toStore) {
       if (isset($this->$toStore) && \is_array($this->$toStore)) {
-        log::debug('array_walk fired');
         $tmpFieldToStore = $this->$toStore;
         if (isset($this->hn)) \array_walk($tmpFieldToStore,['self','storeToAsset'],$this->hn);
         $this->$toStore = $tmpFieldToStore;
@@ -28,14 +26,12 @@ trait StoreToAsset {
   }
 
   protected static function storeToAsset(&$modelValue,$modelKey,$hn) {
-    log::debug('storetoasset fired');
     if (\is_array($modelValue) && isset($modelValue['base64string']) && !isset($modelValue['id'])) {
       if (isset($modelValue['assetType'])) $assetType = $modelValue['assetType'];
       else $assetType = null;
-      log::debug('base64string fired');
       $result = AssetController::addAssetBase64($hn,$modelValue['base64string'],$assetType);
       if ($result['success']) {
-        $modelValue = $result['returnModels'];
+        $modelValue = $result['returnModels'][0];
       }
     }
   }
