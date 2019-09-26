e49f52dfef058a9d963a725a229c1e6473797e48
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-09-26 19:45:16 +0700

    correct classname typo

--- app/Http/Controllers/Encounter/TransactionController.php
+++ app/Http/Controllers/Encounter/TransactionController.php
@@ -198,6 +198,6 @@ class TransactionController extends Controller
           'encounterId' => 'required',
           'productCode' => 'required',
         ];
-        return DataController::createModel($transactions,\App\Models\Patients\PatientsTransactions::class,$validationRule);
+        return DataController::createModel($transactions,\App\Models\Patient\PatientsTransactions::class,$validationRule);
     }
 }
