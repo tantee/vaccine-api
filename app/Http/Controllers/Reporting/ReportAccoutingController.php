4942970ab9c491ca844aa395ab5aaf0507697a87
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-11-02 16:37:57 +0700

    add accounting report

---
+++ app/Http/Controllers/Reporting/ReportAccoutingController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Reporting;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReportAccoutingController extends Controller
+{
+    //
+}
