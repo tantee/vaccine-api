33f8e4077834bd5f784f5c291c7b6ae021c5c8b7
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-11-12 13:59:25 +0700

    Cache for payers, remove laratrust

--- app/Models/Master/Payers.php
+++ app/Models/Master/Payers.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Master;
 
+use Watson\Rememberable\Rememberable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Traits\UserStamps;
@@ -9,11 +10,14 @@ use App\Models\Traits\UserStamps;
 class Payers extends Model
 {
     //
-    use SoftDeletes,UserStamps;
+    use SoftDeletes,UserStamps,Rememberable;
 
     protected $primaryKey = 'payerCode';
     public $incrementing = false;
     protected $keyType = 'string';
     
     protected $guarded = [];
+
+    protected $rememberFor = 60;
+    protected $rememberCacheTag = 'payers_query';
 }
