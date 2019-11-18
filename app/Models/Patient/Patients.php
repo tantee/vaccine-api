da538ebf598aa074220656fefa7509d85bb95e29
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-11-18 09:09:35 +0700

    Fixed to real name

--- app/Models/Patient/Patients.php
+++ app/Models/Patient/Patients.php
@@ -65,7 +65,8 @@ class Patients extends Model
     }
 
     public function getNameRealThAttribute() {
-      return \App\Models\Patient\PatientsNames::where('hn',$this->hn)->where('nameType','TH')->orderBy('id','desc')->first();
+      $name = \App\Models\Patient\PatientsNames::where('hn',$this->hn)->where('nameType','TH')->orderBy('id','desc')->first();
+      return ($name==null) ? $this->name_real_en : $name;
     }
 
     public function getNameRealEnAttribute() {
