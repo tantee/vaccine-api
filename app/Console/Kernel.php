d43c42464e2b775ce230784fef7e6a07f97bdd31
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2019-12-15 08:37:33 +0700

    add return keyword

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->call(function() {
-            \App\Http\Controller\Export\ExportContrller::Export();
+            return \App\Http\Controller\Export\ExportContrller::Export();
         })->everyFiveMinutes()
             ->withoutOverlapping()
             ->onOneServer()
