d83e3d6b5f55e67a85e2db3a89520f824537965d
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-01-02 10:31:41 +0700

    add product type choice

---
+++ database/migrations/2020_01_02_102146_alter_products_change_product_type.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProductsChangeProductType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE products MODIFY productType values ENUM('medicine','supply','procedure','service','laboratory','radiology','package')");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("ALTER TABLE products MODIFY productType values ENUM('medicine','supply','procedure','service','laboratory','radiology')");
+    }
+}
