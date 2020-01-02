05a8eb7c4957a1e8792811dd2081dee714c79802
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-01-03 00:08:12 +0700

    change non login to anonymous user

--- app/Models/Traits/UserStamps.php
+++ app/Models/Traits/UserStamps.php
@@ -8,20 +8,20 @@ trait UserStamps {
   public static function bootUserStamps() {
     static::creating(function($model) {
       if (Auth::guard('api')->check()) $model->created_by = Auth::guard('api')->user()->username;
-      else $model->created_by = '0';
+      else $model->created_by = 'anonymous';
     });
     static::updating(function($model) {
       $original = $model->getOriginal();
       if (array_key_exists('deleted_by',$original) && $model->deleted_by == $original['deleted_by']) {
         if (Auth::guard('api')->check()) $model->updated_by = Auth::guard('api')->user()->username;
-        else $model->updated_by = '0';
+        else $model->updated_by = 'anonymous';
       } else {
         $model->timestamps = false;
       }
     });
     static::deleting(function($model) {
       if (Auth::guard('api')->check()) $model->deleted_by = Auth::guard('api')->user()->username;
-      else $model->deleted_by = '0';
+      else $model->deleted_by = 'anonymous';
       $model->save();
     });
     static::restoring(function($model) {
