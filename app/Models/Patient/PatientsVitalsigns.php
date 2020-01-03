e6cf9c470f49501c656a53f015a2f9fa4b1b21d9
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-01-03 11:51:23 +0700

    change class name

--- app/Models/Patient/PatientsVitalsigns.php
+++ app/Models/Patient/PatientsVitalsigns.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Traits\UserStamps;
 
-class PatientsVitalSigns extends Model
+class PatientsVitalsigns extends Model
 {
     use SoftDeletes,UserStamps;
 
