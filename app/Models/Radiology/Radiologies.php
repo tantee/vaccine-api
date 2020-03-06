5c81a8c1d8f60a90c9a1fc7daab43c3979b01fde
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-03-07 00:41:43 +0700

    add relation and with

--- app/Models/Radiology/Radiologies.php
+++ app/Models/Radiology/Radiologies.php
@@ -11,4 +11,22 @@ class Radiologies extends Model
     use SoftDeletes,UserStamps;
 
     protected $guarded = [];
+
+    public function Patient() {
+      return $this->belongsTo('App\Models\Patient\Patients','hn','hn');
+    }
+
+    public function Report() {
+      return $this->hasOne('App\Models\Document\Documents','id','reportDocumentId');
+    }
+
+    public function Request() {
+      return $this->hasOne('App\Models\Document\Documents','id','requestDocumentId');
+    }
+
+    public function Doctor() {
+        return $this->hasOne('App\Models\Master\Doctors','doctorCode','reportingDoctor');
+    }
+
+    protected $with = ['Doctor'];
 }
