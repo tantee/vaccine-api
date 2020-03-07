b415114c2d346382eedeac1875abd2c801a47de3
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-03-07 14:04:57 +0700

    change with config

--- app/Models/Radiology/Radiologies.php
+++ app/Models/Radiology/Radiologies.php
@@ -40,16 +40,5 @@ class Radiologies extends Model
         return $this->hasOne('App\Models\Master\Doctors','doctorCode','reportingDoctorCode');
     }
 
-    public function toArray()
-    {
-        $toArray = parent::toArray();
-
-        $toArray['reportDocument'] = $this->report_document;
-        $toArray['requestDocument'] = $this->request_document;
-        $toArray['reportingDoctor'] = $this->reporting_doctor;
-
-        return $toArray;
-    }
-
-    protected $with = ['reporting_doctor'];
+    protected $with = ['reportingDoctor','requestDocument','reportDocument'];
 }
