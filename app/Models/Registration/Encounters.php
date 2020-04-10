fb09d6e721f71b4d95819ba38715d7d91a159037
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-04-10 21:53:22 +0700

    cast admit and discharge as date

--- app/Models/Registration/Encounters.php
+++ app/Models/Registration/Encounters.php
@@ -123,13 +123,18 @@ class Encounters extends Model
 
         static::updated(function($model){
             $original = $model->getOriginal();
-            if (($original['dischargeDateTime']==null && $model->dischargeDateTime!==null) || ($original['dischargeDateTime']!=$model->dischargeDateTime)) {
+            if (($original['dischargeDateTime']==null && $model->dischargeDateTime!==null) || ($original['dischargeDateTime']!=$model->attributes['dischargeDateTime'])) {
                 if ($model->encounterType == 'IMP') \App\Http\Controllers\Encounter\IPDController::autoRoundDischarge($model->encounterId);
             }
         });
 
         parent::boot();
     }
+
+    protected $dates = [
+        'admitDateTime',
+        'dischargeDateTime'
+    ];
 
     protected $casts = [
         'locationLog' => 'array',
