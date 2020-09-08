132518093f83a7c550f0285f1ac88d503fc06462
Poompis Pattaranutaporn <poompis.pat@canceralliance.co.th>
2020-09-08 23:50:02 +0700

    migration for stock request

---
+++ database/migrations/2020_09_02_141707_create_stocks_dispensing_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksDispensingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks_dispensings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('stocksRequestId')->index()->nullable();
+            $table->string('productCode');
+            $table->integer('quantity');
+            $table->integer('stockTo')->nullable();
+            $table->integer('stockFrom')->nullable();
+            $table->string('lotNo')->nullable();
+            $table->string('status')->default('prepared');
+            $table->json('statusLog')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks_dispensings');
+    }
+}
